3ca9dd72c23f57ddcf55254d12e45c1626fed65b
hiromirhondalee <106946484+hiromirhondalee@users.noreply.github.com>
2022-10-09 15:48:22 +0900

    Added links to profile website, github and netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ function App() {
   return (
     <div className="App">
       <h1>Weather Application</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://cheerful-faun-c6f164.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hiromi Tanimura{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/hiromirhondalee/react-weather-application"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://fancy-sable-a3b4af.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
