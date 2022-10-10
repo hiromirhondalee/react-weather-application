0bfa3d9a277b43a921d7beb0ce184477fcbed44f
hiromirhondalee <106946484+hiromirhondalee@users.noreply.github.com>
2022-10-10 15:23:56 +0900

    Created basic HTML structure and CSS, added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://cheerful-faun-c6f164.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hiromi Tanimura{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/hiromirhondalee/react-weather-application"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://fancy-sable-a3b4af.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div clasName="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://cheerful-faun-c6f164.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hiromi Tanimura{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/hiromirhondalee/react-weather-application"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://fancy-sable-a3b4af.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,36 @@
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Tokyo</h1>
+      <ul>
+        <li>Mon, 10 Oct 2022, 14:56</li>
+        <li>Broken Clouds</li>
+        <li>
+          <img
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155576/clouds-clipart-md.png"
+            width="100px"
+            alt="broken clouds"
+          />
+        </li>
+        <li>22°C</li>
+      </ul>
+      <hr />
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>💦 Humidity:</li>
+            <li>88%</li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>💨 Wind:</li>
+            <li>8 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
